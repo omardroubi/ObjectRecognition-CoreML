bb98aae809e9fe73c7a387558db06f712080321f
Omar Droubi <omardroubi@Omars-MBP.lan>
2018-04-11 21:32:37 +0300

    Files added

---
+++ ObjectRecognition-CoreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ObjectRecognition-CoreML
+//
+//  Created by Omar Droubi on 4/11/18.
+//  Copyright © 2018 Omar Droubi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
